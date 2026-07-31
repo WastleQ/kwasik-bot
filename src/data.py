6d4d92d784e84e61da39f6c3079d88b7808e712c
Wastle <wastleAW@ya.ru>
2026-07-31 21:54:41 +0300

    Expand item assortment (potions, weapons, armor, accessories) and update dungeon drops

--- src/data.py
+++ src/data.py
@@ -4,11 +4,16 @@ ITEMS = {
     # Зелья
     "hp_potion": {"name": "Зелье здоровья", "type": "use", "heal": 150, "price": 50},
     "mp_potion": {"name": "Зелье маны", "type": "use", "restore_mp": 100, "price": 50},
+    "greater_hp_potion": {"name": "Большое зелье здоровья", "type": "use", "heal": 400, "price": 150},
+    "greater_mp_potion": {"name": "Большое зелье маны", "type": "use", "restore_mp": 300, "price": 150},
     
     # Оружие
     "iron_sword": {"name": "Железный меч", "type": "weapon", "slot": "weapon", "bonus_str": 5, "price": 200},
+    "knight_sword": {"name": "Рыцарский меч", "type": "weapon", "slot": "weapon", "bonus_str": 12, "bonus_agi": 3, "price": 500},
+    "knight_spear": {"name": "Рыцарское копьё", "type": "weapon", "slot": "weapon", "bonus_str": 20, "price": 900},
     "kasaka_dagger": {"name": "Кинжал Ракаша", "type": "weapon", "slot": "weapon", "bonus_str": 15, "bonus_agi": 5, "price": 0},
     "igris_sword": {"name": "Меч Игриса", "type": "weapon", "slot": "weapon", "bonus_str": 30, "price": 0},
+    "demon_dagger": {"name": "Демонический кинжал", "type": "weapon", "slot": "weapon", "bonus_str": 45, "bonus_agi": 15, "price": 0},
     
     # Артефакты
     "orb_of_avarice": {
@@ -19,9 +24,13 @@ ITEMS = {
         "magic_boost": 2.0, 
         "price": 0
     },
+    "ring_of_power": {"name": "Кольцо силы", "type": "weapon", "slot": "accessory", "bonus_str": 8, "bonus_vit": 5, "price": 800},
+    "amulet_of_wisdom": {"name": "Амулет мудрости", "type": "weapon", "slot": "accessory", "bonus_int": 15, "bonus_sen": 10, "price": 1000},
     
     # Броня
     "leather_armor": {"name": "Кожаная броня", "type": "armor", "slot": "armor", "bonus_vit": 5, "price": 200},
+    "steel_armor": {"name": "Стальная броня", "type": "armor", "slot": "armor", "bonus_vit": 15, "price": 600},
+    "shadow_cloak": {"name": "Плащ тени", "type": "armor", "slot": "armor", "bonus_vit": 20, "bonus_agi": 10, "price": 1200},
     "ice_cloak": {"name": "Ледяной плащ", "type": "armor", "slot": "armor", "bonus_vit": 10, "bonus_int": 5, "price": 0}
 }
 
@@ -40,22 +49,22 @@ DUNGEONS = {
     "3": {
         "name": "Заброшенные копи (Ранг C)",
         "mobs": [("Горный тролль", 1500, False), ("Орк-надзиратель", 1800, False), ("Верховный Орк (БОСС)", 4500, True)],
-        "min_dmg": 70, "max_dmg": 120, "boss_drop": "hp_potion", "mob_drop": "iron_sword"
+        "min_dmg": 70, "max_dmg": 120, "boss_drop": "knight_sword", "mob_drop": "greater_hp_potion"
     },
     "4": {
         "name": "Храм Теней (Ранг B)",
         "mobs": [("Теневой солдат", 6000, False), ("Теневой Командир (БОСС)", 15000, True)],
-        "min_dmg": 200, "max_dmg": 350, "boss_drop": "leather_armor", "mob_drop": "iron_sword"
+        "min_dmg": 200, "max_dmg": 350, "boss_drop": "steel_armor", "mob_drop": "greater_mp_potion"
     },
     "5": {
         "name": "Врата Хаоса (Ранг A)",
         "mobs": [("Высший орк", 25000, False), ("Повелитель Пламени (БОСС)", 70000, True)],
-        "min_dmg": 500, "max_dmg": 900, "boss_drop": "orb_of_avarice", "mob_drop": "iron_sword"
+        "min_dmg": 500, "max_dmg": 900, "boss_drop": "demon_dagger", "mob_drop": "ring_of_power"
     },
     "6": {
         "name": "Остров Чеджу (Ранг S)",
         "mobs": [("Мутант-муравей", 100000, False), ("Король Муравьев (БОСС)", 400000, True)],
-        "min_dmg": 1800, "max_dmg": 3500, "boss_drop": "hp_potion", "mob_drop": "iron_sword"
+        "min_dmg": 1800, "max_dmg": 3500, "boss_drop": "amulet_of_wisdom", "mob_drop": "knight_spear"
     }
 }
 
