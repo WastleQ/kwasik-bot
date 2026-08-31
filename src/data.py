ac8fbd973f18d7c30cb7351707fb850b47d81edf
Wastle <wastleAW@ya.ru>
2026-08-31 20:04:24 +0300

    balance: further reduce magic spell damage multipliers (fireball 1.3, lightning 1.7, meteor 2.2)

--- src/data.py
+++ src/data.py
@@ -243,19 +243,19 @@ SPELLS = {
     "шар": {
         "name": "Огненный шар",
         "mp_cost": 50,
-        "damage_mult": 2.0,
+        "damage_mult": 1.3,
         "type": "attack",
     },
     "молния": {
         "name": "Удар молнии",
         "mp_cost": 100,
-        "damage_mult": 2.5,
+        "damage_mult": 1.7,
         "type": "attack",
     },
     "метеор": {
         "name": "Метеорит",
         "mp_cost": 300,
-        "damage_mult": 3.0,
+        "damage_mult": 2.2,
         "type": "attack",
     },
     "хил": {"name": "Исцеление", "mp_cost": 40, "heal": 250, "type": "heal"},
